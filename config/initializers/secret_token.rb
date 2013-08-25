b93b68cb4faec4634f1b12e2eb3c8ce1f09e233d
Todd Pickell <myappleguy@gmail.com>
2013-08-25 07:03:43 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VaporbaseDemo::Application.config.secret_key_base = 'af14861a74688ed96bf02c6285c94523f0c308c87bc1726c3ef68a0c3b744858caa4c549f168162926a5360103b9aedec55cd6adfaecc5c791fcc369b3cff4c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VaporbaseDemo::Application.config.session_store :cookie_store, key: '_vaporbase_demo_session'
